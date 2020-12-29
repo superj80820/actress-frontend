88ece86e6e094a83900f53a3f9d597393510158e
york <york@bitoex.com>
2020-12-30 01:52:07 +0800

    fix: AD will director to sticker store

--- src/components/AdCard.tsx
+++ src/components/AdCard.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
-    margin: '10px'
+    margin: "10px",
   },
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9,
-    marginTop:'30'
+    paddingTop: "56.25%", // 16:9,
+    marginTop: "30",
   },
 });
 
@@ -46,7 +46,12 @@ export default function AdCard() {
         </CardContent>
       </CardActionArea>
       <CardContent>
-        <Button href="https://store.line.me/stickershop/product/12619201" variant="outlined">點選贊助</Button>
+        <Button
+          href="https://line.me/R/shop/sticker/detail/12619201"
+          variant="outlined"
+        >
+          點選支持
+        </Button>
       </CardContent>
     </Card>
   );
