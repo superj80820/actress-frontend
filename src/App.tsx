d5f329628ee4a297cb5e448742131bdaf4988673
york <york@bitoex.com>
2020-12-30 01:50:56 +0800

    feature: Get query from liff

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,45 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import StarCard from './components/StarCard'
-import AdCard from './components/AdCard'
-import AppBar from './components/AppBar'
-import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import StarCard from "./components/StarCard";
+import AdCard from "./components/AdCard";
+import AppBar from "./components/AppBar";
+import "./App.css";
 
 const useStyles = makeStyles({
   root: {
-    display:'flex',
-    justifyContent:'center'
+    display: "flex",
+    justifyContent: "center",
   },
   card: {
-    'flex-direction':'column'
-  }
+    "flex-direction": "column",
+  },
 });
 
+const LIFFQuerys: { [ID: string]: string } = ((liffState) =>
+  liffState !== null
+    ? liffState
+        .slice(2)
+        .split("&")
+        .reduce(
+          (a, b) => ({
+            ...a,
+            ...(([key, value]) => ({
+              [key]: value,
+            }))(b.split("=")),
+          }),
+          {}
+        )
+    : {})(new URLSearchParams(window.location.search).get("liff.state"));
 
-const urlParams = new URLSearchParams(window.location.search)
 // Workaround: 如果搜尋ID不存在需導到錯誤頁面
-const ID: string = urlParams.get('ID') || '1'
+const ID: string = LIFFQuerys.ID || "1";
 
 function App() {
   const classes = useStyles();
   return (
     <div className="App">
       <AppBar></AppBar>
-      <div className={classes.root} style={{ padding: '60px'}}>
+      <div className={classes.root} style={{ padding: "60px" }}>
         <div className={classes.card}>
           <AdCard></AdCard>
           <StarCard ID={ID}></StarCard>
