e237198e3947770ba8526db1571d90854fec63ec
York lin <superj80820@gmail.com>
2022-02-02 20:06:53 +0800

    fix info url

--- src/repository/face-service.ts
+++ src/repository/face-service.ts
@@ -8,11 +8,11 @@ export interface star {
 }
 
 export default async function getInfoByID(ID: string): Promise<star> {
-  const response = await fetch(`${URL}/faces/${ID}`)
+  const response = await fetch(`${URL}/faces/face/${ID}`)
   if (!response.ok) {
     throw new Error(response.statusText)
   }
-  // yorkworkaround: 前端需更改成專用info的API並非使用face+info的API
+  // yorkworkaround: 前端需更改成專用info的API並非使用face+info的API
   const responseJSON = (await response.json())[0]
   return {
     id: parseInt(ID),
